31cf0c575e2a8e4fac354132c99dc1ef1bd6b95a
Ronny Hsieh <1020ronny@gmail.com>
2021-02-07 21:58:34 +0800

    feat(meme): create meme page

--- src/views/Meme.vue
+++ src/views/Meme.vue
@@ -1,35 +1,59 @@
 <template>
   <div class="container">
-    <div>Welcome to Meme</div>
-    <div>{{ resultJson }}</div>
+    <div v-if="loading" class="text-center">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+    <div v-else class="row row-cols-1 row-cols-md-4 g-4">
+      <div v-for="meme of memes" :key="meme.id" class="col">
+        <div class="card">
+          <a :href="meme.imageUrl" target="_blank" rel="noopener noreferrer">
+            <img
+              :src="meme.imageUrl"
+              class="card-img-top"
+              :alt="meme.imageUrl"
+            />
+          </a>
+          <div class="card-tile">
+            <h4>
+              <span
+                v-for="(keyword, index) of meme.keywords"
+                :key="index"
+                class="badge bg-secondary m-2"
+              >
+                {{ keyword }}
+              </span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { useQuery } from '@vue/apollo-composable'
+import { useQuery, useResult } from '@vue/apollo-composable'
 import { gql } from 'apollo-boost'
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
-    const resultJson = ref('')
-
-    onMounted(async () => {
-      const { result } = useQuery(gql`
-        query getUsers {
-          Memes {
-            id
-            imageUrl
-            keywords
-          }
+    const { result, loading } = useQuery(gql`
+      query getUsers {
+        Memes {
+          id
+          imageUrl
+          keywords
         }
-      `)
+      }
+    `)
 
-      resultJson.value = JSON.stringify(result)
-    })
+    const memes = useResult(result, null, (data) => data.Memes)
 
     return {
-      resultJson,
+      loading,
+      memes,
     }
   },
 })
